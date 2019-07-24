989ac445e03ae62dc8bc4019d47f7e6e7abe0b59
gaobing <teqingrenyuan@gmail.com>
2019-07-24 16:44:42 -0700

    using boilerplate code to begin

--- README.md
+++
@@ -1,4 +0,0 @@
-# building-a-react-app
-Create a Grails app with a React view
-
-[Guide](https://guides.grails.org/building-a-react-app/guide/index.html)
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+//tag::garageEntry[]
+var path = require('path');
+
+module.exports = {
+  entry: {
+
+    main: './src/main/webapp/App.js',
+
+    garage: './src/main/webapp/app/Garage.js' // <2>
+  },
+  //end::garageEntry[]
+  //tag::outputSection[]
+  output: {
+    path: path.join(__dirname, 'grails-app/assets/javascripts'),
+    publicPath: '/assets/',
+    filename: 'bundle-[name].js'
+  },
+  //end::outputSection[]
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'src/main/webapp'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["react", ["es2015", {"modules": false}]],
+            plugins: ["transform-class-properties"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: {
+          loader: 'url-loader?limit=10000&prefix=assets/!img'
+        }
+      }
+    ]
+  }
+};
+
